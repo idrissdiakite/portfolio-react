caa222c1eaf6757acd20adc360c7bae11fc301d3
idrissdiakite <idriss.diakite@hotmail.fr>
2021-10-07 20:25:01 +0200

    fix cursor bugs

--- src/components/Cursor/index.jsx
+++ src/components/Cursor/index.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
 
 import "./style.scss";
 
 const Cursor = () => {
+  const cursorRef = React.useRef(null);
 
-    const cursorRef = React.useRef(null);
+  React.useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      // on écoute/récupère le mouvement de la souris
+      const { clientX, clientY } = e;
+      // console.log(e)
+      const mouseX = clientX - cursorRef.current.clientWidth / 2; // permet d'obtenir la position centrale de l'élément
+      const mouseY = clientY - cursorRef.current.clientHeight / 2;
 
-    React.useEffect(() => {
-        document.addEventListener('mousemove', (e) => { // on écoute/récupère le mouvement de la souris 
-            const {clientX, clientY} = e;
-            // console.log(e)
-            const mouseX = clientX - cursorRef.current.clientWidth / 2; // permet d'obtenir la position centrale de l'élément 
-            const mouseY = clientY - cursorRef.current.clientHeight / 2;
+      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+      // console.log(cursorRef)
+    });
+  }, []);
 
-            cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-            // console.log(cursorRef)
-        })
-    }, [])
+  return (
+    <motion.div
+      className="cursor"
+      ref={cursorRef}
+      initial="initial"
+      animate="in"
+      variants={variants}
+      transition={cursorTransition}
+    ></motion.div>
+  );
+};
 
-    return (
-        <div className="cursor" ref={cursorRef}>
-            
-        </div>
-    );
+// Motion
+const variants = {
+  initial: {
+    opacity: 0,
+    scale: 1,
+  },
+  in: {
+    opacity: 1,
+    x: 0,
+  },
+};
+
+const cursorTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 5,
 };
 
 export default Cursor;
