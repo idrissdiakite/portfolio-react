5f9afd954b4acd1f6c3cfb79ec1f173701edf3fa
idrissdigitalcover <i.diakite@digital-cover.fr>
2023-12-29 14:09:55 +0100

    🎨 [WHOIAM] Updated content + style

--- src/components/Whoiam/index.js
+++ src/components/Whoiam/index.js
@@ -9,11 +9,11 @@ const Me = () => {
       <h1 className="whoiam__title">Qui je suis</h1>
       <motion.p
         className="whoiam__suptitle"
-        initial={{ opacity: 0, x: '-100%' }}
-        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ type: "tween", ease: "anticipate", duration: 2 }}
       >
-        10 points:
+        Me, myself and I en 10 points:
       </motion.p>
       <div className="whoiam__wrapper">
         <Infos />
--- src/components/Whoiam/infos.js
+++ src/components/Whoiam/infos.js
@@ -15,43 +15,43 @@ import {
 
 const infos = [
   {
-    icon: <IoLocationOutline className="icon" />,
+    icon: <IoLocationOutline />,
     text: "Né à Annecy, vis actuellement à Lyon.",
   },
   {
-    icon: <IoHeartOutline className="icon" />,
+    icon: <IoHeartOutline />,
     text: "Passionné de musique, d'informatique, de cinéma, de sport et d'art en général.",
   },
   {
-    icon: <IoSchoolOutline className="icon" />,
-    text: "Également titulaire d'un BTS Management des Unités Commerciales.",
+    icon: <IoSchoolOutline />,
+    text: "Titulaire d'un BTS Management des Unités Commerciales.",
   },
   {
-    icon: <IoEarOutline className="icon" />,
+    icon: <IoEarOutline />,
     text: "Toujours à l'écoute, excellent relationnel, sens aïgu des responsabilités.",
   },
   {
-    icon: <IoSunnyOutline className="icon" />,
+    icon: <IoSunnyOutline />,
     text: "Ouvert d'esprit, curieux, créatif et autonome.",
   },
   {
-    icon: <IoStarOutline className="icon" />,
-    text: "Goût prononcé pour le travail en équipe, l'UI/UX et les raclettes en été.",
+    icon: <IoStarOutline />,
+    text: "Goût prononcé pour le travail en équipe, l'ui/ux et les raclettes en été.",
   },
   {
-    icon: <IoMusicalNotesOutline className="icon" />,
+    icon: <IoMusicalNotesOutline />,
     text: <span>Mélomane de naissance et accessoirement curator sur <a href="https://open.spotify.com/user/ilovethistrack.com" target="_blank" rel="noreferrer">Spotify</a>.</span>,
   },
   {
-    icon: <IoStopwatchOutline className="icon" />,
+    icon: <IoStopwatchOutline />,
     text: "Sportif, cours une à deux fois par semaine.",
   },
   {
-    icon: <IoEarthOutline className="icon" />,
-    text: "A vécu 6 mois à Londres et déjà visité 4 continents.",
+    icon: <IoEarthOutline />,
+    text: "Globe-trotter, a vécu 6 mois à Londres et déjà visité 5 continents.",
   },
   {
-    icon: <IoPawOutline className="icon" />,
+    icon: <IoPawOutline />,
     text: "Doublement Papa, d'un petit garçon et d'un petit beagle.",
   },
 ];
