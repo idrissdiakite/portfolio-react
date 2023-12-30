4a3f3f536b88feb729871a00e1e963373375f032
idrissdigitalcover <i.diakite@digital-cover.fr>
2023-12-30 15:19:47 +0100

    🎨 [EXPERIENCES] Updated content

--- src/components/Experiences/index.js
+++ src/components/Experiences/index.js
@@ -13,7 +13,7 @@ const Experiences = () => {
         animate={{ opacity: 1 }}
         transition={{ type: "tween", ease: "anticipate", duration: 2 }}
       >
-        Après plus de 10 années passées dans l'univers de la vente, j'ai décidé fin 2019 de quitter mon poste de manager adjoint afin de me professionnaliser dans le développement web. Titulaire du Titre Professionnel "Développeur web et web mobile full stack" (bac +2), je suis actuellement en cdi au sein de l'agence Digital Cover en tant que développeur créatif.
+        Après plus de 10 années passées dans l'univers de la vente, j'ai décidé fin 2019 de quitter mon poste de manager adjoint afin de me professionnaliser dans le développement web. Titulaire du Titre Professionnel "Développeur web et web mobile full stack" (bac +2), je suis actuellement en cdi au sein de l'agence Digital Cover.
       </motion.p>
       <motion.div
         className="experiences__wrapper"
