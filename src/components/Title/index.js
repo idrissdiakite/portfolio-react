5d2050ebe8747ed71804efb4de3b4a85b4f7c26c
idrissdigitalcover <i.diakite@digital-cover.fr>
2024-01-25 17:42:51 +0100

    💫 [TITLE] Added anim (opacity + translate y on scroll)

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
 const Title = ({ className, children }) => {
+  const controls = useAnimation()
+  const [scrollDirection, setScrollDirection] = useState('down')
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const yOffset = window.scrollY
+      console.log(yOffset)
+      setScrollDirection(yOffset > 0 ? 'up' : 'down')
+      controls.start({
+        y: yOffset * 0.6,
+        opacity: yOffset > 0 ? 0.1 : 1
+      })
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [controls])
+
   return (
     <motion.h1
       className={className}
-      // initial={{ x: '-150%' }}
-      // animate={{ x: 0 }}
-      transition={{ type: 'tween', ease: 'easeOut', duration: 1.5, delay: 0.5 }}
+      initial={{ y: 0, opacity: 1 }}
+      animate={controls}
+      transition={{ 
+        opacity: { type: 'tween', ease: 'easeOut', duration: 0.5 },
+        y: { type: 'tween', ease: 'linear', duration: 0.1 },
+        skewX: { type: 'tween', ease: 'easeOut', duration: 1 }, // Transition spécifique pour skewX
+        skewY: { type: 'tween', ease: 'easeOut', duration: 1 } //
+       }}
     >
       {children}
     </motion.h1>
@@ -15,3 +41,18 @@ const Title = ({ className, children }) => {
 };
 
 export default Title;
+
+// const Title = ({ className, children }) => {
+//   return (
+//     <motion.h1
+//       className={className}
+//       initial={{ x: '-150%' }}
+//       animate={{ x: 0 }}
+//       transition={{ type: 'tween', ease: 'easeOut', duration: 0.5, delay: 0.25 }}
+//     >
+//       {children}
+//     </motion.h1>
+//   );
+// };
+
+// export default Title;
