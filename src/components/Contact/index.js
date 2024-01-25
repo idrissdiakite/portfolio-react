0059880783841ab6ab725e9f3c2336c64c0490c2
idrissdigitalcover <i.diakite@digital-cover.fr>
2024-01-25 17:40:27 +0100

    🎨 [CONTACT] Updated content

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -14,8 +14,8 @@ const Contact = () => {
         animate={{ opacity: 1 }}
         transition={{ type: "tween", ease: "anticipate", duration: 3 }}
       >
-        <p>Actuellement en cdi au sein de l'agence Digital Cover.</p>
-        <p>Ouvert à toutes propositions - développeur front-end/créatif.</p>
+        <p>Potentiellement disponible à partir d'avril (2024).</p>
+        <p>Ouvert aux propositions - développeur front-end/créatif.</p>
       </motion.div>
       <Details />
     </>
