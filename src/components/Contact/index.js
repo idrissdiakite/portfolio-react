1a1b3a0bc6e1796bd52da33f93aef5fbaf6926f6
idrissdigitalcover <i.diakite@digital-cover.fr>
2024-01-30 11:56:22 +0100

    💫 [CONTACT] Deleted anim on suptitle

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Title from "../Title";
-import { motion } from "framer-motion";
 import Details from "./details";
 import "./style.scss";
 
@@ -8,15 +7,10 @@ const Contact = () => {
   return (
     <>
       <Title className="contact__title">Contact</Title>
-      <motion.div 
-        className="contact__suptitle"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ type: "tween", ease: "anticipate", duration: 3 }}
-      >
-        <p>Potentiellement disponible à partir d'avril (2024).</p>
+      <div className="contact__suptitle">
+        <p>À la recherche d'un nouveau challenge.</p>
         <p>Ouvert aux propositions - développeur front-end/créatif.</p>
-      </motion.div>
+      </div>
       <Details />
     </>
   )
